5460777d3b454c8b57046600869fe04f82a6f064
David Ayala <sgprdev@hotmail.com>
2024-11-24 07:55:20 -0500

    to main directly

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// toggle mobile menu
+
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
     const mobileMenu =document.querySelector('.navbar .mobile-menu-items');
@@ -6,3 +8,15 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenu.classList.toggle('active');
     })
 })
+
+// change navbar background on scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+  
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scroll');
+    } else {
+      navbar.classList.remove('navbar-scroll');
+    }
+  });
+  
